cc99dbaa8e54d93c53a15a69caf09a6fe2a0df04
Efriel <efriel@ymail.com>
2021-01-17 14:27:42 +0700

    change axios url

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
@@ -156,6 +156,7 @@ Register.propTypes = {
     auth: state.auth,
     errors: state.errors
   });
+  
   export default connect(
     mapStateToProps,
     { registerUser }
--- src/reducers/authReducers.js
+++ src/reducers/authReducers.js
@@ -1,13 +1,14 @@
 import {
     SET_CURRENT_USER,
     USER_LOADING
-  } from "../actions/types";
-  const isEmpty = require("is-empty");
-  const initialState = {
-    isAuthenticated: false,
-    user: {},
-    loading: false
-  };
+} from "../actions/types";
+
+const isEmpty = require("is-empty");
+const initialState = {
+  isAuthenticated: false,
+  user: {},
+  loading: false
+};
   export default function(state = initialState, action) {
     switch (action.type) {
       case SET_CURRENT_USER:
