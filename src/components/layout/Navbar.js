1047545032f319bc05660d9a05d1385206556171
Efriel <efriel@ymail.com>
2021-01-18 11:04:55 +0700

    add operational and master nav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -10,38 +10,91 @@ class Navbarheader extends Component {
     return (
         <Fragment>
         <Navbar color="faded" light expand="md">
+          
+          <Nav className="ml-auto" nav  bar>
+          
+          <NavItem className="d-flex align-items-center">
+            <NavLink className="font-weight-bold" href="#">
+              Home
+            </NavLink>
+          </NavItem>
+
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Master
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                Part
+              </DropdownItem>
+              <DropdownItem>
+                Finished Good
+              </DropdownItem>                
+              <DropdownItem>
+                Site
+              </DropdownItem>
+              <DropdownItem>
+                Supplier
+              </DropdownItem>                
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Procurement
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                Request 
+              </DropdownItem>
+              <DropdownItem>
+                Vendor Invoice
+              </DropdownItem>                
+              <DropdownItem>
+                PO to Vendor
+              </DropdownItem>
+              <DropdownItem>
+                Vendor Receipt
+              </DropdownItem>                
+              <DropdownItem divider />
+              <DropdownItem>
+                Cust. Order
+              </DropdownItem>                
+              <DropdownItem>
+                Cust. Invoice
+              </DropdownItem>                
+              <DropdownItem>
+                Cust. Receipt
+              </DropdownItem>                
+              <DropdownItem>
+                Sent Item
+              </DropdownItem>                
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Use and Mutation
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                Form Use
+              </DropdownItem>
+              <DropdownItem>
+                Form Mutation
+              </DropdownItem>                
+              <DropdownItem>
+                Delivery Note
+              </DropdownItem>              
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          </Nav>
 
           <NavbarBrand href="/">
             MIS
           </NavbarBrand>
-          <Nav className="ml-auto" nav  bar>
 
-          <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Master
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Part
-                </DropdownItem>
-                <DropdownItem>
-                  Finished Good
-                </DropdownItem>                
-                <DropdownItem>
-                  Site
-                </DropdownItem>
-                <DropdownItem>
-                  Supplier
-                </DropdownItem>                
-              </DropdownMenu>
-            </UncontrolledDropdown>
-
-            <NavItem className="d-flex align-items-center">
-              <NavLink className="font-weight-bold" href="#">
-                Home
-              </NavLink>
-            </NavItem>
-          </Nav>
         </Navbar>
       </Fragment>
     );
