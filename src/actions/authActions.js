de356011583fc0bf28819ecf4b2baf8084f53ce4
Efriel <efriel@ymail.com>
2021-01-26 10:42:58 +0700

    Add error return login data, CORS testing 1

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -38,12 +38,13 @@ export const loginUser = userData => dispatch => {
       const decoded = jwt_decode(token);      
       dispatch(setCurrentUser(decoded));      
     })
-    .catch(err =>
+    .catch(err => {
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
-      })      
-    );
+        payload: err.response
+      });
+      console.log(error.response);
+    });
 };
 // Set logged in user
 export const setCurrentUser = decoded => {
